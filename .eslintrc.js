73e6830bbab27a0e62a3c9dd3357c72f7c7a4725
ytk6565 <ytk6565@gmail.com>
2021-02-18 01:14:20 +0900

    feat: eslint-plugin-layout-shift

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,8 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
-  plugins: ['prettier', 'vue-a11y'],
-  // add your custom rules here
-  rules: {},
+  plugins: ['prettier', 'vue-a11y', '@mizdra/layout-shift'],
+  rules: {
+    '@mizdra/layout-shift/require-size-attributes': 2,
+  },
 }
