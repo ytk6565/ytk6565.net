90e5020ef3c0e28c3a36f770ea5984792743d9d9
ytk6565 <ytk6565@gmail.com>
2021-02-20 00:26:30 +0900

    fix: html lang

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,9 @@ const nuxtConfiguration: NuxtConfig = {
    */
   head: {
     title: process.env.npm_package_name || '',
+    htmlAttrs: {
+      lang: 'ja',
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
