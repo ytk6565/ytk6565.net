1ba435820b28b10c5545c11ac85ffd2997cd0501
ytk6565 <ytk6565@gmail.com>
2021-06-26 17:52:16 +0900

    change the installation command from yarn to npm

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,14 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue-a11y/recommended',
     '@nuxtjs/eslint-config-typescript',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
-  plugins: ['prettier', 'vue-a11y', '@mizdra/layout-shift'],
+  plugins: ['prettier', '@mizdra/layout-shift'],
   rules: {
     '@mizdra/layout-shift/require-size-attributes': 2,
   },
-}
+};
