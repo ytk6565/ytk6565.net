7f4b789fed2a4a9f19c9266650600bb8440fa40b
Yutaka Kaneko <ytk6565@gmail.com>
2023-01-21 22:15:28 +0900

    fix: google analytics id (#21)

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -35,9 +35,7 @@ export default defineConfig(({ mode }) => ({
       },
     }),
     VitePluginRadar({
-      analytics: {
-        id: 'UA-186919639-2',
-      },
+      analytics: [{ id: 'UA-186919639-3' }, { id: 'G-56DM5BZZ30' }],
     }),
     tsconfigPaths(),
   ],
